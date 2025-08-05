e95027d460cde69f88b05d2d9b5f943a1a96fe14
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2025-08-05 10:32:44 +0800

    Initial commit

---
+++ generator.py
@@ -0,0 +1,22 @@
+import random
+
+print("Good day to you.")
+
+ans = input("Let's generate our quests for the day? ").capitalize()
+if ans == "Yes":
+    with open("quests.txt", "r", encoding="utf-8") as q:
+        quests = q.readlines()
+
+else:
+    print("Goodbye and good luck to you.")
+    SystemExit
+
+ques = []
+
+for quest in quests:
+    ques.append(quest)
+
+print("Here is your quest for today: ", end="")
+print(random.choice(ques))
+    
+
