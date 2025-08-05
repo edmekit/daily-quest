2eb9b91517c83173c5cafd03b2bad5f2ccb2d6b4
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2025-08-05 10:39:53 +0800

    fix algo

--- generator.py
+++ generator.py
@@ -6,17 +6,19 @@ ans = input("Let's generate our quests for the day? ").capitalize()
 if ans == "Yes":
     with open("quests.txt", "r", encoding="utf-8") as q:
         quests = q.readlines()
+    ques = []
+
+    for quest in quests:
+        ques.append(quest)
+
+    print("Here is your quest for today: ", end="")
+    print(random.choice(ques))
 
 else:
+    print()
     print("Goodbye and good luck to you.")
     SystemExit
 
-ques = []
 
-for quest in quests:
-    ques.append(quest)
-
-print("Here is your quest for today: ", end="")
-print(random.choice(ques))
     
 
