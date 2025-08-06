c34f5a74601fe6202d5b3998adfce39978cc47f3
Edmarc Mekitpekit <mekitpekit.eggmarc@gmail.com>
2025-08-06 10:33:45 +0800

    fix algoa and pygame

--- generator.py
+++ generator.py
@@ -19,24 +19,12 @@ def make_btn(pos_x, pos_y, width, height, color, text, font_size):
     disp.blit(message, message_rect)
     return btn_rect
 
-def display_text(text, x, y, size):
-    font = pygame.font.Foont("freesansbold.ttf", size)
-    text_surf = font.render(text, True, black)
+def display_text(text, x, y, size, color):
+    font = pygame.font.Font("freesansbold.ttf", size)
+    text_surf = font.render(text, True, color)
     disp.blit(text_surf, (x,y))
 
-def get_input():
-    typed = ""
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                return typed
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RETURN:
-                    return typed
-                elif event.key == pygame.K_BACKSPACE:
-                    typed = typed[:-1]
-                else:
-                    typed += event.unicode
+
 
 
 
@@ -44,38 +32,48 @@ def get_input():
 def ui():
     run = True
     start = False
+    typing = True
+    typed = ""
+    with open("quests.txt", "r", encoding="utf-8") as q:
+        quests = q.readlines()
+    ques = []
+    for quest in quests:
+        ques.append(quest)
+    quest_for_today = random.choice(ques)
+            
     while run:
+        disp.fill(white)
+          
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
-        display_text("Get daily quest", 300, 100, 50)
-        #get inut
-        #validate input
-        #flag to start
-        #open file
-        #shuffle
-        #display quest
+            
+            if typing:
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RETURN:
+                        typing = False
+                        start = True
+                    elif event.key == pygame.K_BACKSPACE:
+                        typed = typed[:-1]
+                    else:
+                        typed += event.unicode
         
+              
+        if typing:
+            display_text("Get daily quest.", 200, 100, 50, black)
+            display_text(typed, 300, 200, 50, black)
+
+        elif start:
+            disp.fill(black)
+
+            display_text(quest_for_today, 100, 200, 15, white)
+            
+            
+        pygame.display.update()
 
 
-
-ans = input("Let's generate our quests for the day? ").capitalize()
-if ans == "Yes":
-    with open("quests.txt", "r", encoding="utf-8") as q:
-        quests = q.readlines()
-    ques = []
-
-    for quest in quests:
-        ques.append(quest)
-
-    print("Here is your quest for today: ", end="")
-    print(random.choice(ques))
-
-else:
-    print()
-    print("Goodbye and good luck to you.")
-    SystemExit
+ui()
 
 
     
